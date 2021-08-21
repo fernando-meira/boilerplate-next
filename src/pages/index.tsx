a32d4ecfc900cf4483415c1f3565b7f2b18d3a64
fernando-meira <thomwork@outlook.com>
2021-08-21 01:16:34 -0300

    feat: 🎸 Added config of the prettier, eslint and editorconfig

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
 
-export default function Home() {
+export default function Home({ title = 'React Avançado' }) {
+  const foo = 'bla';
+
   return (
     <div className={styles.container}>
       <Head>
@@ -12,9 +14,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <h1 className={styles.title}>{title}</h1>
 
         <p className={styles.description}>
           Get started by editing{' '}
@@ -65,5 +65,5 @@ export default function Home() {
         </a>
       </footer>
     </div>
-  )
+  );
 }
