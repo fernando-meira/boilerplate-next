4958aa3215cd754b97d87b252afe6bf6282c877e
fernando-meira <thomwork@outlook.com>
2021-08-24 01:19:27 -0300

    feat: 🎸 Configured PWA

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,7 +8,9 @@ function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>Boilerplate Next.js</title>
-
+        <link rel="shortcut icon" href="/img/icon-512.png" />
+        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#06092b" />
         <meta
           name="description"
